8688469484a9843036470537b92c0b734510ad61
Dmitry Kornatovskiy <75861742+DFKorn@users.noreply.github.com>
2025-03-01 14:25:13 +0300

    add my solution

--- solutions/7.sql
+++ solutions/7.sql
@@ -3,6 +3,13 @@
   SELECT * FROM albums where release_year IS NULL;
 */
 
+-- Mine
+UPDATE albums
+SET release_year = 1986
+WHERE albums.id > 0 and release_year is NULL;
+
+
+-- Solution
 UPDATE albums
 SET release_year = 1986
 WHERE id = 4;
